cfb14af853e5e317adbdf5c09dc0790578eaf281
Stefan-Gabriel <stefan.gabriel.muscalu@gmail.com>
2023-09-28 21:25:11 +0300

    update: customization and fixes

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -10,57 +10,90 @@ const Hero = () => (
     <HeroAvatar
       title={
         <>
-          Hi there, I'm <GradientText>Ixartz</GradientText> 👋
+          Hi there, I'm <GradientText>Ștefan</GradientText>{' '}
+          <div className="inline-block animate-bounce">👋</div>
         </>
       }
       description={
         <>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus{' '}
-          <a className="text-cyan-400 hover:underline" href="/">
-            malesuada
+          Joined{' '}
+          <a
+            className="text-cyan-400 hover:underline"
+            href="https://web.archive.org/web/20171002062424/https://findie.co/"
+          >
+            Findie
           </a>{' '}
-          nisi tellus, non imperdiet nisi tempor at. Lorem ipsum dolor sit amet,{' '}
-          <a className="text-cyan-400 hover:underline" href="/">
-            consectetur
+          from it's inception in 2014 aiming to provide a Netflix for shorts
+          platform. Pivoted to{' '}
+          <a
+            className="text-cyan-400 hover:underline"
+            href="https://kamua.com/"
+          >
+            Kamua
           </a>{' '}
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+          around 2019, an AI centric video editing platform. Got acquitted by{' '}
+          <a
+            className="text-cyan-400 hover:underline"
+            href="https://jellysmack.com/"
+          >
+            JellySmack 🦄
+          </a>{' '}
+          in 2021.
+          <br />
+          Currently tasked with creating internal video editing tools, ML & AI
+          pipelines, and other <i>special ✨ projects</i>.
+          <br />
+          Over 9+ years of experience in the video space, building workflows,
+          AI, and products.
+          <br />
+          <br />
+          By day, I'm a technologist. Always looking to create meaningful tech.
+          <br />
+          By night, I'm a gamer and a tinkerer. Always eager to learn new
+          things.
         </>
       }
       avatar={
         <img
-          className="h-80 w-64"
-          src="/assets/images/avatar.svg"
+          className="h-64 w-64 rounded-3xl"
+          src="/assets/images/avatar.png"
           alt="Avatar image"
           loading="lazy"
         />
       }
       socialButtons={
-        <>
-          <a href="/">
+        <div className="mb-8 flex w-full flex-row justify-center md:mb-0 md:justify-start">
+          <a href="https://github.com/legraphista">
             <HeroSocial
-              src="/assets/images/twitter-icon.png"
+              src="/assets/images/github-icon.svg"
+              alt="Github icon"
+            />
+          </a>
+          <a href="https://twitter.com/legraphista">
+            <HeroSocial
+              src="/assets/images/twitter-icon.svg"
               alt="Twitter icon"
             />
           </a>
-          <a href="/">
+          {/* <a href="/"> */}
+          {/*  <HeroSocial */}
+          {/*    src="/assets/images/facebook-icon.png" */}
+          {/*    alt="Facebook icon" */}
+          {/*  /> */}
+          {/* </a> */}
+          <a href="https://www.linkedin.com/in/stefangabriel/">
             <HeroSocial
-              src="/assets/images/facebook-icon.png"
-              alt="Facebook icon"
-            />
-          </a>
-          <a href="/">
-            <HeroSocial
-              src="/assets/images/linkedin-icon.png"
+              src="/assets/images/linkedin-icon.svg"
               alt="Linkedin icon"
             />
           </a>
-          <a href="/">
+          <a href="https://www.youtube.com/channel/UCODTTNnONsrZUnLnz3WiZaw">
             <HeroSocial
-              src="/assets/images/youtube-icon.png"
+              src="/assets/images/youtube-icon.svg"
               alt="Youtube icon"
             />
           </a>
-        </>
+        </div>
       }
     />
   </Section>
--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -10,57 +10,75 @@ const ProjectList = () => (
   <Section
     title={
       <>
-        Recent <GradientText>Projects</GradientText>
+        My <GradientText>Projects</GradientText>
       </>
     }
   >
     <div className="flex flex-col gap-6">
       <Project
-        name="Project 1"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
+        name="Joi Tester"
+        description="Quick joi schema validation tool, complete with localized error messages."
+        link="https://joi-tester.corneroftheinternet.rocks"
         img={{
-          src: '/assets/images/project-web-design.png',
-          alt: 'Project Web Design',
+          src: '/assets/images/projects/joi-tester.png',
+          alt: 'Joi Tester',
         }}
         category={
           <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.LIME}>Web design</Tags>
-            <Tags color={ColorTags.SKY}>Tailwind.css</Tags>
-            <Tags color={ColorTags.ROSE}>TypeScript</Tags>
+            <Tags color={ColorTags.FUCHSIA}>React</Tags>
+            <Tags color={ColorTags.LIME}>Typescript</Tags>
+            <Tags color={ColorTags.SKY}>Joi</Tags>
           </>
         }
       />
       <Project
-        name="Project 2"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
-        img={{ src: '/assets/images/project-fire.png', alt: 'Project Fire' }}
+        name="Destiny 2 Weapon Breakdown"
+        description="Useful for inventory management and disposal of redundant weapons. Helps you find duplicates, easily compare them across damage type, weapon type, archetype and slot."
+        link="https://d2-weapons-breakdown.pages.dev/"
+        img={{
+          src: '/assets/images/projects/d2-weapon-breakdown.png',
+          alt: 'Destiny 2 Weapon Breakdown',
+        }}
         category={
           <>
-            <Tags color={ColorTags.VIOLET}>Next.js</Tags>
-            <Tags color={ColorTags.EMERALD}>Blog</Tags>
-            <Tags color={ColorTags.YELLOW}>JavaScript</Tags>
+            <Tags color={ColorTags.FUCHSIA}>React</Tags>
+            <Tags color={ColorTags.LIME}>Typescript</Tags>
+            <Tags color={ColorTags.ROSE}>MUI</Tags>
           </>
         }
       />
       <Project
-        name="Project 3"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
+        name="Destiny 2 Vendor Armor"
+        description="Helps you easily check this week's vendors armor stats at a quick glance across all vendors and get indication of best roles."
         link="/"
-        img={{ src: '/assets/images/project-maps.png', alt: 'Project Maps' }}
+        img={{
+          src: '/assets/images/projects/d2-vendors.png',
+          alt: 'Destiny 2 Vendor Armor',
+        }}
         category={
           <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.INDIGO}>Bootstrap</Tags>
-            <Tags color={ColorTags.ROSE}>TypeScript</Tags>
+            <Tags color={ColorTags.FUCHSIA}>React</Tags>
+            <Tags color={ColorTags.LIME}>Typescript</Tags>
+            <Tags color={ColorTags.ROSE}>MUI</Tags>
+          </>
+        }
+      />
+      <Project
+        name="CautaMedicamente.ro"
+        description="A tool to seach the available stock of medicine in pharmacies near you. Built on top of each pharmacy's own inventory and stock system. The project currently covers only Romania and aims to have all pharmacies encolled in the search. Currently this is in standby due to lack of personal time."
+        link="https://alpha.cautamedicamente.ro/"
+        img={{
+          src: '/assets/images/projects/cautamedicamente.png',
+          alt: 'CautaMedicamente.ro',
+        }}
+        category={
+          <>
+            <Tags color={ColorTags.RED}>WIP</Tags>
+            <Tags color={ColorTags.FUCHSIA}>React</Tags>
+            <Tags color={ColorTags.LIME}>Typescript</Tags>
+            <Tags color={ColorTags.ROSE}>MUI</Tags>
+            <Tags color={ColorTags.ORANGE}>CF Workers / D1</Tags>
+            <Tags color={ColorTags.CYAN}>DBSCAN</Tags>
           </>
         }
       />
